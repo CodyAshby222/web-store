83f29f01643e55f8c3c472c9314a515cabd63227
John <jashby@student.neumont.edu>
2021-10-19 14:15:46 -0600

    orders updated

--- client/src/Components/orders.js
+++ client/src/Components/orders.js
@@ -1,65 +1,54 @@
-import React, { Component} from 'react';
-import { Redirect } from 'react-router';
-import OrderItem from './orderItemComponent';
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router";
+import OrderItem from "./orderItemComponent";
 
-class Orders extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            orderList: [],
-            redirect: null
-        };
+const Orders = ({ closeMenu, loggedIn, userKey }) => {
+  const [orderList, setOrderList] = useState([]);
+  const [redirect, setRedirect] = useState(null);
+
+  useEffect(() => {
+    closeMenu();
+    if (!loggedIn) {
+      setRedirect("/login");
+    } else {
+      fetch(`http://localhost:3001/orders?key=${userKey}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setOrderList(data);
+          console.log(data);
+        });
     }
+  }, [closeMenu, loggedIn, userKey]);
 
-
-    componentDidMount() {
-        this.props.closeMenu();
-        if(!this.props.loggedIn){
-            this.setState({
-                redirect: "/login"
-            });
-        }
-        else {
-            fetch(`http://localhost:3001/orders?key=${this.props.userKey}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                this.setState({
-                orderList: data
-                });
-                console.log(data);
-            });
-        }
-    }
-
-    render() {
-        if(!this.props.loggedIn) {
-            return <Redirect to="/" />
-        }
-        if(this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-        }
-        return(
-            <section className="mainSection">
-                <h1 className="pageTitle">Orders</h1>
-                <section id="orderContainer" className="tableContainer">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Quantity</th>
-                                <th>Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.orderList.map(orderItem => (<OrderItem orderItem={orderItem} />))}
-                        </tbody>
-                    </table>
-                </section>
-            </section>
-        );
-    }
-}
+  if (!loggedIn) {
+    return <Redirect to="/" />;
+  }
+  if (redirect) {
+    return <Redirect to={redirect} />;
+  }
+  return (
+    <section className="mainSection">
+      <h1 className="pageTitle">Orders</h1>
+      <section id="orderContainer" className="tableContainer">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orderList.map((orderItem) => (
+              <OrderItem orderItem={orderItem} />
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </section>
+  );
+};
 
 export default Orders;
