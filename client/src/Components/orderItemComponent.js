be0b554d0a4d16d9fa3b046d56eb23dad412ed5b
John <jashby@student.neumont.edu>
2021-10-19 14:02:41 -0600

    orderItemComponent updated

--- client/src/Components/orderItemComponent.js
+++ client/src/Components/orderItemComponent.js
@@ -1,26 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class OrderItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  formatDate = (date) => {
+const OrderItem = ({ orderItem }) => {
+  const formatDate = (date) => {
     return (
       date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear()
     );
   };
 
-  render() {
-    return (
-      <tr>
-        <td>{this.props.orderItem.name}</td>
-        <td>{this.props.orderItem.quantity}</td>
-        <td>{this.formatDate(new Date(this.props.orderItem.date))}</td>
-      </tr>
-    );
-  }
-}
+  return (
+    <tr>
+      <td>{orderItem.name}</td>
+      <td>{orderItem.quantity}</td>
+      <td>{formatDate(new Date(orderItem.date))}</td>
+    </tr>
+  );
+};
 
 export default OrderItem;
