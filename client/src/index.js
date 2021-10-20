9a1138b242fce8d6dbf95a036374689626021c5e
John <jashby@student.neumont.edu>
2021-10-20 13:59:10 -0600

    style updates

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
+import "./styles/button.css";
+import "./styles/cart.css";
+import "./styles/landing.css";
+import "./styles/nav.css";
+import "./styles/font.css";
+import "./styles/login.css";
+import "./styles/orders.css";
+import "./styles/product.css";
 import App from "./App";
 
 ReactDOM.render(
