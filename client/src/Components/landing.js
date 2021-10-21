e238f75fc2987ea11e0c07c12290567a44db0c4c
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-10-20 18:00:25 -0600

    css work for landing

--- client/src/Components/landing.js
+++ client/src/Components/landing.js
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from "react";
 
-
-function landing({closeMenu}) {
-    return (
-        <section className="landing__mainContent">
-            <div>
-                <h1>Wear A Mask</h1>
-
-                <div>
-                    <p>Wearing a mask helps prevent the spread of Covid-19</p>
-                </div>
-
-            </div>
-            <div className="landing__images">
-                <img src="/"></img>
-
-            </div>
-        </section>
-    )
+function landing({ closeMenu }) {
+  return (
+    <section className="landing__mainContent ">
+      <div className="landing__content">
+        <h1>Wear A Mask</h1>
+        <div>
+          <p>Wearing a mask helps prevent the spread of Covid-19</p>
+          <div className="landing__getStartedButton">Get Started</div>
+        </div>
+      </div>
+      <div className="landing__images">
+        <img className="peopleImg" src="/mask_models.png"></img>
+        <img className="orangeBackgroundImg" src="/orange_background.png"></img>
+      </div>
+    </section>
+  );
 }
 
-export default landing
+export default landing;
