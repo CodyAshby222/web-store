34caca34afdc750b978718d6b26f79aead684dc9
Dylan <60489717+DRoberts919@users.noreply.github.com>
2021-10-21 14:56:36 -0600

    landing page done

--- client/src/Components/landing.js
+++ client/src/Components/landing.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function landing({ closeMenu }) {
   return (
     <section className="landing__mainContent ">
       <div className="landing__content">
-        <h1>Wear A Mask</h1>
+        <h1>Wear</h1>
+        <h1>A Mask</h1>
         <div>
           <p>Wearing a mask helps prevent the spread of Covid-19</p>
-          <div className="landing__getStartedButton">Get Started</div>
+        </div>
+        <div className="landing__buttonContainer">
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            <div className="landing__getStartedButton">Get Started</div>
+          </Link>
         </div>
       </div>
       <div className="landing__images">
-        <img className="peopleImg" src="/mask_models.png"></img>
-        <img className="orangeBackgroundImg" src="/orange_background.png"></img>
+        <img className="orangeBackgroundImg" src="/models_with_bg.png"></img>
       </div>
     </section>
   );
